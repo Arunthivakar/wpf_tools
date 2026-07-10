43847b26453b28a652fc5822698cf185b34bb2dd
Arunthivakar <mailtoarunthivakar@gmail.com>
2026-07-10 18:49:13 +0530

    custom wpf control - buttonAdv

---
+++ ButtonAdv/Controls/ButtonControls/Button/Implementation/ButtonAdv.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ATAV.Tools.WPF_2026.Controls.ButtonControls.Button.Implementation
+{
+    internal class ButtonAdv : ButtonBase, ICommandSource, IButtonAdv
+    {
+        #region Initialization
+        #region Constants
+        private const double SmallIconHeight = 16.0;
+        private const double SmallIconWidth = 16.0;
+        private const double NormalIconHeight = 16.0;
+        private const double NormalIconWidth = 16.0;
+        private const double LargeIconHeight = 26.0;
+        private const double LargeIconWidth = 26.0;
+        #endregion
+        #endregion
+        public ButtonAdv()
+        {
+            //DefaultStyleKey = typeof(ButtonAdv);
+            Initialize();
+        }
+
+        #region Private Variables
+        private AccessText accessText;
+        private ContentPresenter smallIconContent;
+        private ContentPresenter largeIconContent;
+
+        #endregion
+
+        #region Dependency Properties
+        public bool IsCancel
+        {
+            get { return (bool)GetValue(IsCancelProperty); }
+            set { SetValue(IsCancelProperty, value); }
+        }
+        public static readonly DependencyProperty IsCancelProperty =
+    DependencyProperty.Register("IsCancel", typeof(bool), typeof(ButtonAdv), new PropertyMetadata(new PropertyChangedCallback(OnIsCancelChanged)));
+
+        public bool IsDefault
+        {
+            get { return (bool)GetValue(IsDefaultProperty); }
+            set { SetValue(IsDefaultProperty, value); }
+        }
+        public static readonly DependencyProperty IsDefaultProperty =
+    DependencyProperty.Register("IsDefault", typeof(bool), typeof(ButtonAdv), new PropertyMetadata(new PropertyChangedCallback(OnIsDefaultChanged)));
+
+        public Stretch IconStretch
+        {
+            get { return (Stretch)GetValue(IconStretchProperty); }
+            set { SetValue(IconStretchProperty, value); }
+        }
+        public static readonly DependencyProperty IconStretchProperty =
+    DependencyProperty.Register("IconStretch", typeof(Stretch), typeof(ButtonAdv), new PropertyMetadata(Stretch.Uniform));
+
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+        public static readonly DependencyProperty LabelProperty =
+    DependencyProperty.Register("Label", typeof(string), typeof(ButtonAdv), new PropertyMetadata("Button"));
+
+        public DataTemplate IconTemplate
+        {
+            get { return (DataTemplate)GetValue(IconTemplateProperty); }
+            set { SetValue(IconTemplateProperty, value); }
+        }
+        public static readonly DependencyProperty IconTemplateProperty =
+    DependencyProperty.Register("IconTemplate", typeof(DataTemplate), typeof(ButtonAdv), new PropertyMetadata(null));
+
+        public DataTemplateSelector IconTemplateSelector
+        {
+            get { return (DataTemplateSelector)GetValue(IconTemplateSelectorProperty); }
+            set { SetValue(IconTemplateSelectorProperty, value); }
+        }
+        public static readonly DependencyProperty IconTemplateSelectorProperty =
+    DependencyProperty.Register("IconTemplateSelector", typeof(DataTemplateSelector), typeof(ButtonAdv), new PropertyMetadata(null));
+
+        public bool IsCheckable
+        {
+            get { return (bool)GetValue(IsCheckableProperty); }
+            set { SetValue(IsCheckableProperty, value); }
+        }
+        public static readonly DependencyProperty IsCheckableProperty =
+    DependencyProperty.Register("IsCheckable", typeof(bool), typeof(ButtonAdv), new PropertyMetadata(false, new PropertyChangedCallback(OnIsCheckableChanged)));
+
+        public bool IsChecked
+        {
+            get { return (bool)GetValue(IsCheckedProperty); }
+            set { SetValue(IsCheckedProperty, value); }
+        }
+        public static readonly DependencyProperty IsCheckedProperty =
+   DependencyProperty.Register("IsChecked", typeof(bool), typeof(ButtonAdv), new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, new PropertyChangedCallback(OnIsCheckedChanged)));
+
+        public ImageSource LageIcon
+        {
+            get { return (ImageSource)GetValue(LargeIconProperty); }
+            set { SetValue(LargeIconProperty, value); }
+        }
+        public static readonly DependencyProperty LargeIconProperty =
+    DependencyProperty.Register("LargeIcon", typeof(ImageSource), typeof(ButtonAdv), new PropertyMetadata(null));
+
+        public ImageSource SmallIcon
+        {
+            get { return (ImageSource)GetValue(SmallIconProperty); }
+            set { SetValue(SmallIconProperty, value); }
+        }
+        public static readonly DependencyProperty SmallIconProperty =
+    DependencyProperty.Register("SmallIcon", typeof(ImageSource), typeof(ButtonAdv), new PropertyMetadata(null));
+
+        public double IconWidth
+        {
+            get { return (double)GetValue(IconWidthProperty); }
+            set { SetValue(IconWidthProperty, value); }
+        }
+        public static readonly DependencyProperty IconWidthProperty =
+    DependencyProperty.Register("IconWidth", typeof(double), typeof(ButtonAdv), new PropertyMetadata(16.0, new PropertyChangedCallback(OnSizeChanged)));
+
+        public double IconHeight
+        {
+            get { return (double)GetValue(IconHeightProperty); }
+            set { SetValue(IconHeightProperty, value); }
+        }
+        public static readonly DependencyProperty IconHeightProperty =
+    DependencyProperty.Register("IconHeight", typeof(double), typeof(ButtonAdv), new PropertyMetadata(16.0, new PropertyChangedCallback(OnSizeChanged)));
+
+        public bool IsMultiLine
+        {
+            get { return (bool)GetValue(IsMultiLineProperty); }
+            set { SetValue(IsMultiLineProperty, value); }
+        }
+        public static readonly DependencyProperty IsMultiLineProperty =
+    DependencyProperty.Register("IsMultiLine", typeof(bool), typeof(ButtonAdv), new PropertyMetadata(true));
+
+        public SizeMode SizeMode
+        {
+            get { return (SizeMode)GetValue(SizeModeProperty); }
+            set { SetValue(SizeModeProperty, value); }
+        }
+        public static readonly DependencyProperty SizeModeProperty =
+    DependencyProperty.Register("SizeMode", typeof(SizeMode), typeof(ButtonAdv), new PropertyMetadata(SizeMode.Medium, new PropertyChangedCallback(OnSizeChanged)));
+        #endregion
+
+        #region Implementation
+        private void Initialize()
+        {
+            UpdateSize();
+
+        }
+        public void UpdateSize()
+        {
+
+        }
+        private static void OnIsCheckableChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        private void OnIsCheckableChanged()
+        {
+            if (IsChecked)
+                IsChecked = false;
+            OnIsCheckedChanged();
+        }
+        private static void OnIsCheckedChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        private void OnIsCheckedChanged()
+        {
+
+        }
+        private static void OnSizeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        private void OnSizeChanged()
+        {
+            UpdateSize();
+        }
+        private static void OnIsCancelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        private static void OnIsDefaultChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region Events
+        #endregion
+
+        #region Overrides
+        #endregion
+    }
+    public enum SizeMode
+    {
+        Small,
+        Medium,
+        Large
+    }
+}
---
+++ ButtonAdv/Controls/ButtonControls/Interface/IButtonAdv.cs
@@ -0,0 +1,17 @@
+﻿using ATAV.Tools.WPF_2026.Controls.ButtonControls.Button.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media;
+
+namespace ATAV.Tools.WPF_2026.Controls
+{
+    internal interface IButtonAdv
+    {
+        string Label { get; set; }
+        ImageSource LageIcon { get; set; }
+        ImageSource SmallIcon { get; set; }
+        bool IsMultiLine { get; set; }
+        SizeMode SizeMode { get; set; }
+    }
+}
